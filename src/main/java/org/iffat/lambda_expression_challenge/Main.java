bc0434474980bcef3ba0a12ab28e1f39457df215
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-03-26 06:37:08 +0700

    08. Lambda Expression Challenge

---
+++ src/main/java/org/iffat/lambda_expression_challenge/Main.java
@@ -0,0 +1,44 @@
+package org.iffat.lambda_expression_challenge;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+        String[] names = {"Anna", "Bob", "Carole", "David", "Ed", "Fred", "Gary"};
+
+        Arrays.setAll(names, (i) -> {
+            System.out.println(i);
+            return names[i].toUpperCase();
+        });
+        System.out.println("--> Transform to Uppercase");
+        System.out.println(Arrays.toString(names));
+
+        List<String> backedByArray = Arrays.asList(names);
+
+        backedByArray.replaceAll(s -> s += " " + getRandomChar('A', 'D') + ".");
+        System.out.println("--> Add random middle initial");
+        System.out.println(Arrays.toString(names));
+
+        backedByArray.replaceAll(s -> s += " " + getReversedName(s.split(" ")[0]));
+        System.out.println("--> Add reversed name last name");
+        Arrays.asList(names).forEach(s -> System.out.println(s));
+
+        List<String> newList = new ArrayList<>(List.of(names));
+        newList.removeIf(s -> s.substring(0, s.indexOf(" ")).equals(s.substring(s.lastIndexOf(" ") + 1)));
+        System.out.println("--> Remove names where first = last");
+        newList.forEach(s -> System.out.println(s));
+    }
+
+    public static char getRandomChar(char startChar, char endChar) {
+        return (char) random.nextInt((int) startChar, (int) endChar);
+    }
+
+    private static String getReversedName(String firstName) {
+        return new StringBuilder(firstName).reverse().toString();
+    }
+}
