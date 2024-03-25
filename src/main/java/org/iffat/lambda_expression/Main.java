a7450914ca3cbbfe291a3894347e1389a6ccc881
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-03-25 15:28:46 +0700

    03. Lambda expression, syntax, and additional examples

---
+++ src/main/java/org/iffat/lambda_expression/Main.java
@@ -0,0 +1,26 @@
+package org.iffat.lambda_expression;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(List.of(
+                "alpha", "bravo", "charlie", "delta"
+        ));
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+        System.out.println("-".repeat(10));
+        list.forEach((String myString) -> System.out.println(myString));
+
+        System.out.println("-".repeat(10));
+        String prefix = "nato";
+        list.forEach((var myString) -> {
+            char first = myString.charAt(0);
+            System.out.println(prefix + " " + myString + " means " + first);
+        });
+    }
+}
